f09660c23b52315decfe269364ceb8b70dbf0cf0
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-01-19 03:55:01 +0100

    i added function of export data and exit programe and also i improve my code

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import os
 import re
 import datetime
 from tkinter.font import nametofont
-
+import csv
+from tkinter import filedialog
 
 # Function For check for Data
 def Check_data(data, data_type):
@@ -28,11 +29,19 @@ def Check_data(data, data_type):
             return True
         else:
             return False
+    elif data_type=="Name":
+        if re.match(r'^[A-Za-z\s]+$',data):
+            return True
+        else:
+            return False
     else:
         return False
 
 
+
 # function to add data to json file
+def exit_program():
+    root.destroy()
 def add_json():
     # get data from entry
     product_id = Myid_entre.get()
@@ -56,6 +65,10 @@ def add_json():
     if product_id and not Check_data(product_id,"ID"):
         messagebox.showerror("Error","Please enter correct Id")
         return
+    # nom should be str
+    if product_name and not Check_data(product_name,"Name"):
+        messagebox.showerror("Error","Please enter correct Name")
+        return
     #Check if id already exists
     Myjson_file='products.json'
     if os.path.exists(Myjson_file):
@@ -65,19 +78,13 @@ def add_json():
                 if product_id == item['ID']:
                     messagebox.showerror("Error Id","Id already Exist")
                     return
-
-
-
-
-
-
     #Put Data in entry
     Data_Entry = {
         'ID':product_id,
         'Name':product_name,
         'Prix':f"{product_prix} DH",
         'Quantity':product_Qnt,
-        'Date Exep':product_Date_exep,
+        'Date':product_Date_exep,
         'Total':f"{float(product_prix)*int(product_Qnt)} DH"
     }
     Myjson_file = 'products.json'
@@ -113,7 +120,7 @@ def add_to_treeview():
         # insert new datas
         for item in data:
             tree.insert("","end",values=(
-              item['ID'],item['Name'],item['Prix'],item['Quantity'],item['Date Exep'],item['Total']))
+              item['ID'],item['Name'],item['Prix'],item['Quantity'],item['Date'],item['Total']))
 #Function For Update data
 def Update_data():
     selected_item = tree.focus()
@@ -146,6 +153,10 @@ def Update_data():
     if product_id and not Check_data(product_id,"ID"):
         messagebox.showerror("Error","Please enter correct ID")
         return
+    # name should be str
+    if product_name and not Check_data(product_name,"Name"):
+        messagebox.showerror("Error","Please enter correct Name")
+        return
     #json file read
     with open('products.json','r') as file:
         data=json.load(file)
@@ -165,7 +176,7 @@ def Update_data():
             if product_Qnt:
                item['Quantity']=product_Qnt
             if product_Date_exep:
-               item['Date Exep']=product_Date_exep
+               item['Date']=product_Date_exep
             item['Total']=f"{float(item['Prix'])*int(item['Quantity'])} DH"
             break
     with open('products.json','w') as file:
@@ -195,12 +206,45 @@ def Delete_data():
     add_to_treeview()
     messagebox.showinfo("Delete","The product has been successfully deleted.")
 
+#function of export data
+def export_to_csv():
+    Myjson_file = 'products.json'
+
+    if os.path.exists(Myjson_file):
+        with open(Myjson_file, 'r') as file:
+            data = json.load(file)
+
+        # check if data not empty
+        if data:
+            # Ask the user to choose the file location
+            csv_file_path = filedialog.asksaveasfilename(defaultextension=".csv",
+                                                           filetypes=[("CSV files", "*.csv"), ("All files", "*.*")])
+
+            if csv_file_path:
+                with open(csv_file_path, 'w', newline='') as csvfile:
+                    csv_writer = csv.writer(csvfile)
+
+                    # Write header
+                    csv_writer.writerow(['ID', 'Name', 'Prix', 'Quantity', 'Date', 'Total'])
+
+                    # Write data
+                    for item in data:
+                        csv_writer.writerow([item['ID'], item['Name'], item['Prix'], item['Quantity'], item['Date'],
+                                             item['Total']])
+
+                messagebox.showinfo("Info", f'Data has been exported to {csv_file_path}')
+            else:
+                messagebox.showerror('Nothing', "No CSV file created as the user canceled.")
+        else:
+            messagebox.showerror('Nothing', "There is no data to export.")
+
 
 #Create Root (Window)
 root = cs.CTk()
 root.title("MyProgram")
 root.config(background="#393939")
-root.geometry("1000x550")
+root.geometry("1095x575+100+50")
+root.resizable(False,False)
 
 
 
@@ -230,25 +274,31 @@ Quantity = cs.CTkLabel(root,text="Quantity :",font=My_Fonts["Primary_Font"],bg_c
 Quantity.place(x=20,y=200)
 Quantity_entre = cs.CTkEntry(root,font=My_Fonts["Second_Font"],corner_radius=5,bg_color="#393939",border_width=1)
 Quantity_entre.place(x=120,y=200)
+
 #DateExper
 
 #Add Button
 add = cs.CTkButton(root,corner_radius=15,border_width=2,bg_color="#393939",text_color="white",cursor='hand2'
                    ,font=My_Fonts["Second_Font"],hover_color="#3B7A57",text="Add Products",fg_color="#29AB87",width=245,height=40,command=add_json)
-add.place(x=20,y=280)
+add.place(x=20,y=270)
 #Update Buttun
 Update = cs.CTkButton(root,corner_radius=15,border_width=2,bg_color="#393939",text_color="white",cursor='hand2'
                       ,font=My_Fonts["Second_Font"],hover_color="#FFA836",text="Update Products",fg_color="#F5761A",width=245,height=40,command=Update_data)
-Update.place(x=20,y=340)
-#Sell Product
+Update.place(x=20,y=330)
+#Delete Product
 Sell = cs.CTkButton(root,corner_radius=15,border_width=2,bg_color="#393939",text_color="white",cursor='hand2'
-                    ,font=My_Fonts["Second_Font"],hover_color="#FFA836",text="Sell Products",fg_color="#F5761A",width=245,height=40)
-Sell.place(x=20,y=400)
-#Delete Products
-Delete = cs.CTkButton(root,corner_radius=15,border_width=2,bg_color="#393939",text_color="white",cursor='hand2'
                     ,font=My_Fonts["Second_Font"],hover_color="#FF2400",text="Delete Products",fg_color="#ED2939",width=245,height=40,command=Delete_data)
-Delete.place(x=20,y=460)
-#  i will create treeview
+Sell.place(x=20,y=390)
+#Exit Program
+Delete = cs.CTkButton(root,corner_radius=15,border_width=2,bg_color="#393939",text_color="white",cursor='hand2'
+                    ,font=My_Fonts["Second_Font"],hover_color="#FFA836",text="Exit Program",fg_color="#f5761A",width=245,height=40,command=exit_program)
+Delete.place(x=20,y=450)
+# Export Button
+ExportButton = cs.CTkButton(root, corner_radius=15, border_width=2, bg_color="#393939", text_color="white",
+                            cursor='hand2', font=My_Fonts["Second_Font"], hover_color="#008CBA", text="Export CSV",
+                            fg_color="#0056b3", width=245, height=40,command=export_to_csv )
+ExportButton.place(x=20, y=510)
+#  I will create treeview
 style = ttk.Style(root)
 style.theme_use('clam')
 style.configure('Treeview', foreground='black', font=('Open Sans', 18, 'bold'), fieldbackground='#313837',rowheight=35,bordercolor='gray',border=4, lightcolor='gray', darkcolor='gray')
@@ -258,7 +308,7 @@ tree = ttk.Treeview(root,height=22)
 tree['show'] = 'headings'  # This removes the empty first column
 tree['padding'] = [0, 0]
 #Create Columns
-tree['columns'] = ('ID','Name','Prix','Quantity','Date Exep','Total')
+tree['columns'] = ('ID','Name','Prix','Quantity','Date','Total')
 #Create Header of treeview
 TkHeadingFont = nametofont("TkHeadingFont")
 TkHeadingFont.configure(size=20)
@@ -266,7 +316,7 @@ style.configure("Treeview.Heading", foreground="black",font=('Open Sans', 20, 'b
 
 tree.column('#0',width=0,stretch=tk.NO)
 for column in tree['columns']:
-    tree.column(column, anchor=tk.CENTER, width=160,)
+    tree.column(column, anchor=tk.CENTER, width=200,)
 
 for column in tree['columns']:
     tree.heading(column,text=column)
